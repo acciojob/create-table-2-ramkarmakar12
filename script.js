8731364c2ebab84b03558ae5833ad494ee399a69
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-21 22:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function createTable() {
+function insert_Row() {
     //Write your code here
+	let table = document.getElementById("sampleTable");
+			let newRow = table.insertRow(0); // Insert at the first position (top)
+
+			let cell1 = newRow.insertCell(0);
+			let cell2 = newRow.insertCell(1);
+
+			cell1.textContent = "New Cell1"; 
+			cell2.textContent = "New Cell2"; 
+	
+  
   
 }
